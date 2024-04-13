ead77d2c101564714dd5adb10526207657e079fd
AleksandrHovhannisyan <aleksandrhovhannisyan@gmail.com>
2024-04-12 19:19:38 -0500

    Add eslint and lint code

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-const fs = require("fs");
-const path = require("path");
+const fs = require('fs')
+const path = require('path')
 
 /**
  * @typedef EleventyPluginNetlifyRedirectsOptions
@@ -11,32 +11,32 @@ const path = require("path");
 // TODO: figure out how to test this 😬 Even as an inlined template literal string, it isn't testable since it uses Eleventy globals. Maybe there's some way I can run 11ty in a test runner and manually parse the template?
 
 /** Parses and returns the Netlify redirects template. NOTE: This could've technically been inlined, but it's nicer to author it in a separate file with proper syntax highlighting and LSP features. */
-const getRedirects = () => fs.readFileSync(path.resolve(__dirname, "redirects.liquid"), "utf-8");
+const getRedirects = () => fs.readFileSync(path.resolve(__dirname, 'redirects.liquid'), 'utf-8')
 
 /**
  * @param {unknown} eleventyConfig
  * @param {EleventyPluginNetlifyRedirectsOptions} options
  */
 module.exports = (eleventyConfig, options) => {
-    // https://www.11ty.dev/docs/plugins/#feature-testing
-    if(!('addTemplate' in eleventyConfig)) {
-        console.log(`[eleventy-plugin-netlify-redirects] WARN Eleventy plugin compatibility: Virtual Templates are required for this plugin, please use Eleventy v3.0 or newer.`);
-    }
+  // https://www.11ty.dev/docs/plugins/#feature-testing
+  if (!('addTemplate' in eleventyConfig)) {
+    console.log('[eleventy-plugin-netlify-redirects] WARN Eleventy plugin compatibility: Virtual Templates are required for this plugin, please use Eleventy v3.0 or newer.')
+  }
 
-    const { staticRedirects, frontMatterOverrides } = options;
+  const { staticRedirects, frontMatterOverrides } = options
 
-    const frontMatter = {
-        // Write this virtual template to a _redirects file at the root of the output directory
-        permalink: '/_redirects',
-        // https://www.11ty.dev/docs/collections/#how-to-exclude-content-from-collections
-        eleventyExcludeFromCollections: true,
-        // Forward static redirects and other front matter overrides to the template
-        staticRedirects,
-        ...frontMatterOverrides
-    };
+  const frontMatter = {
+    // Write this virtual template to a _redirects file at the root of the output directory
+    permalink: '/_redirects',
+    // https://www.11ty.dev/docs/collections/#how-to-exclude-content-from-collections
+    eleventyExcludeFromCollections: true,
+    // Forward static redirects and other front matter overrides to the template
+    staticRedirects,
+    ...frontMatterOverrides
+  }
 
-    // Needed per https://github.com/11ty/eleventy/issues/1612#issuecomment-2027476340
-    eleventyConfig.addTemplateFormats("liquid");
-    // Virtual template: https://github.com/11ty/eleventy/issues/1612
-    eleventyConfig.addTemplate("./redirects.liquid", getRedirects(), frontMatter);
-};
+  // Needed per https://github.com/11ty/eleventy/issues/1612#issuecomment-2027476340
+  eleventyConfig.addTemplateFormats('liquid')
+  // Virtual template: https://github.com/11ty/eleventy/issues/1612
+  eleventyConfig.addTemplate('./redirects.liquid', getRedirects(), frontMatter)
+}
